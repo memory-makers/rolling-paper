89a294274b0233e6ce9133855157e6ba6560dc78
zellytozelly <zellytozelly@gmail.com>
2022-08-06 01:38:21 +0900

    #1 feat: 공유 모달 인풋 구현

--- src/pages/ModalTest/ModalTest.tsx
+++ src/pages/ModalTest/ModalTest.tsx
@@ -1,15 +1,30 @@
-import { useState } from 'react';
+import { ChangeEventHandler, useState } from 'react';
 
 import Modal from '@/components/Modal';
-import ModalText from '@/components/Modal/ModalItem/ModalText';
+import { ModalInput, ModalText } from '@/components/Modal/ModalItem';
 
 const ModalTest = () => {
   const [, setIsModalOpen] = useState(false);
+  const [title, setTitle] = useState('');
+  const [dueDate, setDueDate] = useState('');
+
+  const handleTitleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setTitle(e.currentTarget.value);
+  };
+
+  const handleDueDateChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setDueDate(e.currentTarget.value);
+  };
 
   return (
     <Modal setIsModalOpen={setIsModalOpen}>
       <ModalText size='title'>롤링 페이퍼를 만들어볼까요?</ModalText>
+
       <ModalText size='label'>롤링페이퍼 이름을 적어주세요</ModalText>
+      <ModalInput type='text' name='title' onChange={handleTitleChange} />
+
+      <ModalText size='label'>언제 열어보시겠어요?</ModalText>
+      <ModalInput type='date' name='dueDate' onChange={handleDueDateChange} />
     </Modal>
   )
 }
