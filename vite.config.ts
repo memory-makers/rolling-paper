10c7ee31ac78d9017e6aa537b4c5bda262e4e0c6
zellytozelly <zellytozelly@gmail.com>
2022-08-05 20:36:35 +0900

    #3 fix: 전역변수 설정 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,4 +16,8 @@ export default defineConfig({
       },
     },
   },
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+  
 });
