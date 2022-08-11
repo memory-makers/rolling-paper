476550f769296ad7a51af041dc8180591ef6c9aa
zellytozelly <zellytozelly@gmail.com>
2022-08-12 00:14:02 +0900

    Feat: 모달 호출 테스트 페이지 생성

---
+++ src/pages/ModalTest/ModalCallTest.tsx
@@ -0,0 +1,20 @@
+import { ModalButton } from '@/components/Modal/ModalItem';
+import { useCallback, useState } from 'react'
+import CreateRoll from '../CreateRoll'
+
+const ModalCallTest = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleClickButton = useCallback(() => {
+    setIsModalOpen((prev) => !prev);
+  }, [isModalOpen]);
+
+  return (
+    <>
+      <ModalButton type='button' onClick={handleClickButton}>ModalCallTest</ModalButton>
+      {isModalOpen && <CreateRoll setIsModalOpen={setIsModalOpen} />}
+    </>
+  )
+}
+
+export default ModalCallTest
