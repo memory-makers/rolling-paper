3fd8acce80fda4efe590e9b12223fb95d13ac568
YumYumNyang <khw121699@gmail.com>
2022-08-14 12:23:22 +0900

    fix:  리팩토링

--- src/components/rollingpaper/Content.tsx
+++ src/components/rollingpaper/Content.tsx
@@ -3,15 +3,23 @@ import React from 'react'
 import Card from './Card'
 import styles from './rollingpaper.module.scss'
 interface ContentProps {
-  handleClick: (index: number) => void
+  handleClickCard: (index: number) => void
 }
 
-const Content = ({ handleClick }: ContentProps) => {
+const Content = ({ handleClickCard }: ContentProps) => {
   return (
     <div className={styles.content}>
       {Object.keys(cardDummy).map((key, index) => {
         const card = cardDummy[key]
-        return <Card index={index} key={key} card={card} handleClick={() => handleClick(index)} />
+        const rotateDeg = index % 3 && index % 4 ? 'rotate(-10deg)' : 'rotate(10deg)'
+        return (
+          <Card
+            rotateDeg={rotateDeg}
+            key={key}
+            card={card}
+            handleClick={() => handleClickCard(index)}
+          />
+        )
       })}
     </div>
   )
