d829a0784a13b66c6796edb40f2d84b15e4bc746
zellytozelly <zellytozelly@gmail.com>
2022-08-15 22:17:15 +0900

    feat: url 공유 모달 구현

--- src/pages/CreateRoll/index.tsx
+++ src/pages/CreateRoll/index.tsx
@@ -2,6 +2,7 @@ import { MouseEventHandler, useState } from 'react'
 
 import Modal from '@/components/Modal'
 import MakeRoll from './contents/MakeRoll'
+import CreateShareRoll from './contents/CreateShareRoll'
 
 interface Props {
   setIsModalOpen: (state: boolean) => void
@@ -12,18 +13,27 @@ const CreateRoll = ({ setIsModalOpen }: Props) => {
   const [dueDate, setDueDate] = useState('')
   const [paperTheme, setPaperTheme] = useState('light')
 
+  const [paperUrl, setPaperUrl] = useState('test')
+  const [isNextStep, setIsNextStep] = useState(false)
+
   const handleButtonClick: MouseEventHandler<HTMLButtonElement> = (e) => {
     console.log(title, dueDate, paperTheme)
+    setPaperUrl(`https://rolling-paper.vercel.app/rollingpaper/${paperUrl}`)
+    setIsNextStep(true)
   }
 
   return (
     <Modal setIsModalOpen={setIsModalOpen}>
-      <MakeRoll
-        handleButtonClick={handleButtonClick}
-        setTitle={setTitle}
-        setDueDate={setDueDate}
-        setPaperTheme={setPaperTheme}
-      />
+      {!isNextStep && (
+        <MakeRoll
+          handleButtonClick={handleButtonClick}
+          setTitle={setTitle}
+          setDueDate={setDueDate}
+          setPaperTheme={setPaperTheme}
+        />
+      )}
+
+      {isNextStep && <CreateShareRoll paperUrl={paperUrl} />}
     </Modal>
   )
 }
