7eb101cf6d68c0f7e29dd8f9b78de35a675569ee
zellytozelly <zellytozelly@gmail.com>
2022-08-18 10:44:15 +0900

    #22 feat: 롤링페이퍼 삭제 레이아웃 구현

--- src/pages/ModalTest/ModalCallTest.tsx
+++ src/pages/ModalTest/ModalCallTest.tsx
@@ -4,6 +4,7 @@ import { ModalButton } from '@/components/Modal/ModalItem'
 import CreateRoll from '../CreateRoll'
 import EditRoll from '../ChangeRoll/EditRoll'
 import ChangeShareRoll from '../ChangeRoll/ChangeShareRoll'
+import DeleteRoll from '../ChangeRoll/DeleteRoll'
 
 const ModalCallTest = () => {
   const [isModalOpen, setIsModalOpen] = useState(false)
@@ -19,9 +20,10 @@ const ModalCallTest = () => {
       </ModalButton>
       {/* {isModalOpen && <CreateRoll setIsModalOpen={setIsModalOpen} />} */}
       {/* {isModalOpen && <EditRoll setIsModalOpen={setIsModalOpen} />} */}
-      {isModalOpen && (
+      {/* {isModalOpen && (
         <ChangeShareRoll setIsModalOpen={setIsModalOpen} paperUrl={'user.paperUrl'} />
-      )}
+      )} */}
+      {isModalOpen && <DeleteRoll setIsModalOpen={setIsModalOpen} />}
     </>
   )
 }
