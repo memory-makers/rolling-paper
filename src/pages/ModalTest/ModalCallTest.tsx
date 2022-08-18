2473d2240f41ab120058015d8d86c50b41a45bf4
zellytozelly <zellytozelly@gmail.com>
2022-08-18 16:57:31 +0900

    #24 feat: 작성자전용 링크공유 레이아웃

--- src/pages/ModalTest/ModalCallTest.tsx
+++ src/pages/ModalTest/ModalCallTest.tsx
@@ -3,6 +3,7 @@ import { useCallback, useState } from 'react'
 import { ModalButton } from '@/components/Modal/ModalItem'
 import CreateRoll from '../CreateRoll'
 import CheckSendingCard from '../WriterRoll/CheckSendingCard'
+import WriterShareRoll from '../WriterRoll/WriterShareRoll'
 
 const ModalCallTest = () => {
   const [isModalOpen, setIsModalOpen] = useState(false)
@@ -17,7 +18,8 @@ const ModalCallTest = () => {
         ModalCallTest
       </ModalButton>
       {/* {isModalOpen && <CreateRoll setIsModalOpen={setIsModalOpen} />} */}
-      {isModalOpen && <CheckSendingCard setIsModalOpen={setIsModalOpen} />}
+      {/* {isModalOpen && <CheckSendingCard setIsModalOpen={setIsModalOpen} />} */}
+      {isModalOpen && <WriterShareRoll setIsModalOpen={setIsModalOpen} paperUrl="user.paperUrl" />}
     </>
   )
 }
