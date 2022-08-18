7a9f80dd25d8d6010f00ff0316645e8d69cce163
zellytozelly <zellytozelly@gmail.com>
2022-08-18 15:43:19 +0900

    #24 feat: 카드 최종전송확인 레이아웃

--- src/pages/ModalTest/ModalCallTest.tsx
+++ src/pages/ModalTest/ModalCallTest.tsx
@@ -2,6 +2,7 @@ import { useCallback, useState } from 'react'
 
 import { ModalButton } from '@/components/Modal/ModalItem'
 import CreateRoll from '../CreateRoll'
+import CheckSendingCard from '../WriterRoll/CheckSendingCard'
 
 const ModalCallTest = () => {
   const [isModalOpen, setIsModalOpen] = useState(false)
@@ -15,7 +16,8 @@ const ModalCallTest = () => {
       <ModalButton type="button" onClick={handleClickButton}>
         ModalCallTest
       </ModalButton>
-      {isModalOpen && <CreateRoll setIsModalOpen={setIsModalOpen} />}
+      {/* {isModalOpen && <CreateRoll setIsModalOpen={setIsModalOpen} />} */}
+      {isModalOpen && <CheckSendingCard setIsModalOpen={setIsModalOpen} />}
     </>
   )
 }
