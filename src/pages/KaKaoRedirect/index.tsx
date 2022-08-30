d411417243371b90b792ea56c87a45108970f66c
jewonyeon <yjw9852@gmail.com>
2022-08-30 19:09:35 +0900

    change-api 2

--- src/pages/KaKaoRedirect/index.tsx
+++ src/pages/KaKaoRedirect/index.tsx
@@ -1,145 +1,54 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
+const BASE_URL = import.meta.env.VITE_BASE_URL
+
 const KakaoRedirect = () => {
-  const AUTH_CODE = new URL(window.location.href).searchParams.get('token')
-  const getKakaoTokenHandler = async (code: string) => {
-    const data: any = {
-      grant_type: 'authorization_code',
-      client_id: import.meta.env.VITE_KAKAO_REST_API_KEY,
-      redirect_uri: import.meta.env.VITE_KAKAO_REDIRECT_URL,
-      code
-    }
-    const queryString = Object.keys(data)
-      .map((k: any) => encodeURIComponent(k) + '=' + encodeURIComponent(data[k]))
-      .join('&')
-
-    //토큰 발급 REST API
-    const response = await axios.post('https://kauth.kakao.com/oauth/token', queryString, {
-      headers: {
-        'Content-type': 'application/x-www-form-urlencoded;charset=utf-8'
-      }
-    })
-
-    console.log(response)
-  }
+  const AUTH_TOKEN = new URL(window.location.href).searchParams.get('token')
 
   useEffect(() => {
-    if (!AUTH_CODE) return
-    getKakaoTokenHandler(AUTH_CODE)
+    if (!AUTH_TOKEN) return
+    console.log(AUTH_TOKEN)
   }, [])
-
-  const [success, setSuccess] = useState(false)
-
-  const token = new URL(window.location.href).searchParams.get('token')
 
   return (
     <div>
-      {/* {`토큰: `} */}
-      {/* <button
-        onClick={async () => {
-          const response = await axios.post(
-            'http://14.39.205.218:8080/card',
-            {
-              cardText: 'text',
-              cardColor: 'color',
-              paperId: '1'
-            },
-            {
-              headers: {
-                Authorization: `Bearer ${token}`
-              }
-            }
-          )
-
-          console.log(response)
-
-          if (response) {
-            setSuccess(true)
-          }
-        }}
-      >
-        헤더에 토큰보내보기
-      </button>
-
-      <button
-        onClick={async () => {
-          const response = await axios.post('http://14.39.205.218:8080/card', {
-            cardText: 'text',
-            cardColor: 'color',
-            paperId: '1',
-            token
-          })
-
-          console.log(response)
-
-          if (response) {
-            setSuccess(true)
-          }
-        }}
-      >
-        Body에 토큰보내보기
-      </button> */}
-
-      <button
-        onClick={async () => {
-          const response = await axios.post('http://14.39.205.218:8080/paper', {
-            paperTitle: 'tommy temp title',
-            theme: 'tommy temp theme'
-          })
-
-          console.log(response)
-
-          if (response) {
-            setSuccess(true)
-          }
-        }}
-      >
-        토큰 없이 페이퍼 만들기
-      </button>
       <button
         onClick={async () => {
           const response = await axios.post(
-            'http://14.39.205.218:8080/paper',
+            `${BASE_URL}/paper`,
             {
               paperTitle: 'tommy temp title',
               theme: 'tommy temp theme'
             },
             {
               headers: {
-                Authorization: `Bearer ${token}`
+                Authorization: `Bearer ${AUTH_TOKEN}`
               }
             }
           )
 
-          console.log(response)
-
           if (response) {
-            setSuccess(true)
+            console.log(response)
           }
         }}
       >
-        토큰헤더 페이퍼 만들기
+        페이퍼 만들기
       </button>
       <button
         onClick={async () => {
-          const response = await axios.get('http://14.39.205.218:8080/paper', {
+          const response = await axios.get(`${BASE_URL}/paper`, {
             headers: {
-              Authorization: `Bearer ${token}`
+              Authorization: `Bearer ${AUTH_TOKEN}`
             }
           })
-
-          console.log(response)
-
           if (response) {
-            setSuccess(true)
+            console.log(response)
           }
         }}
       >
         롤링페이퍼 정보 얻기
       </button>
-      <div>{success && '성공'}</div>
-      <div>{!success && '실패'}</div>
     </div>
   )
 }
