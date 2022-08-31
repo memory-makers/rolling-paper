6dce9444916a3ac7c3203e9a8091e46fc302aae9
kyhyun <k29176@gmail.com>
2022-08-31 19:57:00 +0900

    #32 feat : Add dropdown componenet layout

--- src/pages/MyPage/MyPageItem/MyPageDropdown/index.tsx
+++ src/pages/MyPage/MyPageItem/MyPageDropdown/index.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import styles from './myPageDropdown.module.scss'
 
-const MyPageDropDown = () => {
-  return <div>MyPageDropDown</div>
+const MyPageDropDown = ({ isVisible }: { isVisible: boolean }) => {
+  const handleClickShare = () => {
+    confirm('공유하기를 진행하시겠습니까?')
+  }
+  const handleClickDelete = () => {
+    confirm('삭제하기를 진행하시겠습니까?')
+  }
+  return (
+    <section className={styles.dropdown}>
+      <button type="button" onClick={handleClickShare}>
+        공유하기
+      </button>
+      <button type="button" onClick={handleClickDelete}>
+        삭제하기
+      </button>
+      <button type="button">{isVisible ? '모두 보기' : '나만 보기'}</button>
+    </section>
+  )
 }
 
 export default MyPageDropDown
--- src/pages/MyPage/MyPageItem/index.tsx
+++ src/pages/MyPage/MyPageItem/index.tsx
@@ -1,6 +1,6 @@
 import styles from './myPageItem.module.scss'
 import { useState } from 'react'
-import cx from 'classnames'
+import MyPageDropDown from './MyPageDropdown'
 interface Props {
   id: number
   title: string
@@ -8,27 +8,46 @@ interface Props {
 }
 
 const MyPageItem = ({ user }: { user: Props }) => {
-  const [onDropdown, setOnDropdown] = useState(false)
+  const [isDropdown, setIsDropdown] = useState<boolean>(false)
+  const [isVisible, setIsVisible] = useState(false)
 
-  const onClickDropdownList = () => {
-    setOnDropdown((prev) => !prev)
+  const handleClickDropdownList = () => {
+    setIsDropdown((prev) => !prev)
   }
+
+  const handleClickVisible = () => {
+    setIsVisible((prevState) => !prevState)
+  }
+
   return (
     <div className={styles.myPageMainContent}>
-      <div className={styles.paperNameWrap}>
-        <p>{user.title}</p>
-        <button>
-          <img src="/src/assets/eye-off.svg" alt="눈 가림 버튼" />
-          {/* <img src="/src/assets/eye.svg" alt="눈 보임 버튼" />
-          <img src="/src/assets/lock.svg" alt="자물쇠 잠금 버튼" /> */}
-        </button>
+      <div className={styles.roll}>
+        <div className={styles.paperInfoWrap}>
+          <p>{user.title}</p>
+          <button type="button" onClick={handleClickVisible}>
+            {isVisible ? (
+              <img src="/src/assets/eye.svg" alt="눈 보임 버튼" />
+            ) : (
+              <img src="/src/assets/eye-off.svg" alt="눈 가림 버튼" />
+            )}
+            {/* <img src="/src/assets/lock.svg" alt="자물쇠 잠금 버튼" /> */}
+          </button>
+        </div>
+        <div className={styles.openDateWrap}>
+          <p>{user.dueDate}</p>
+          <button type="button" onClick={handleClickDropdownList}>
+            {isDropdown ? (
+              <>
+                <img src="/src/assets/arrow-down.svg" alt="가리는 토글 버튼" />
+              </>
+            ) : (
+              <img src="/src/assets/arrow-up.svg" alt="보여주는 토글 버튼" />
+            )}
+          </button>
+        </div>
       </div>
-      <div className={styles.openDateWrap}>
-        <p>{user.dueDate}</p>
-        <button type="button" onClick={onClickDropdownList}>
-          {/* <img src="/src/assets/arrow-down.svg" alt="가리는 토글 버튼" /> */}
-          <img src="/src/assets/arrow-up.svg" alt="보여주는 토글 버튼" />
-        </button>
+      <div className={styles.dropdown}>
+        {isDropdown && <MyPageDropDown isVisible={isVisible} />}
       </div>
     </div>
   )
