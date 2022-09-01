8aa61982921834fce303284dae4347bf6793c07f
YumYumNyang <khw121699@gmail.com>
2022-09-01 17:13:40 +0900

    #16 feat: 스티커 컴포넌트 수정중

--- src/utils/rollingPaper/stickerDummy.ts
+++ src/utils/rollingPaper/stickerDummy.ts
@@ -2,7 +2,7 @@ import StickerType from './Sticker.type'
 import { StickerShape } from './Sticker.type'
 
 const stickerDummy: StickerType[] = [
-  { id: '1', type: StickerShape.heart, x: 0, y: 130, paperId: '1' },
+  { id: '1', type: StickerShape.heart, x: 0, y: 130, rotate: 30, scale: 1.5, paperId: '1' },
   { id: '2', type: StickerShape.bear, x: 150, y: 160, rotate: 15, paperId: '1' }
 ]
 
