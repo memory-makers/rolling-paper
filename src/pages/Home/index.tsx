ad875ff6a2f05db924de98c91a7a0b5aa2ac2471
jewonyeon <yjw9852@gmail.com>
2022-09-04 18:56:03 +0900

    #20 feat: kakao redirect 페이지 구현

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,78 +1,77 @@
-import React from "react";
-import KakaoButton from "@/components/buttons/KakaoButton";
-import Card from "@/components/home/Card";
+import React from 'react'
+import KakaoButton from '@/components/buttons/KakaoButton'
+import Card from '@/components/home/Card'
 import colors from '@/styles/colors.module.scss'
 
-import styles from './index.module.scss';
+import styles from './index.module.scss'
 
 const cardOption = [
   {
     content: '사랑해',
     style: {
       transform: 'rotate(-10deg)',
-      backgroundColor: colors.cardRed,
+      backgroundColor: colors.cardRed
     }
   },
   {
     content: '고마워',
     style: {
       transform: 'rotate(10deg)',
-      backgroundColor: colors.cardBlue,
+      backgroundColor: colors.cardBlue
     }
   },
   {
     content: '잘부탁해',
     style: {
       transform: 'rotate(-10deg)',
-      backgroundColor: colors.cardGreen,
+      backgroundColor: colors.cardGreen
     }
   },
   {
     content: '수고했어',
     style: {
       transform: 'rotate(10deg)',
-      backgroundColor: colors.cardPurple,
+      backgroundColor: colors.cardPurple
     }
   },
   {
     content: '오래보자',
     style: {
       transform: 'rotate(10deg)',
-      backgroundColor: colors.cardYellow,
+      backgroundColor: colors.cardYellow
     }
   },
   {
     content: '그리울꺼야',
     style: {
       transform: 'rotate(-10deg)',
-      backgroundColor: colors.cardBeige,
+      backgroundColor: colors.cardBeige
     }
   },
   {
     content: '소중해',
     style: {
       transform: 'rotate(-10deg)',
-      backgroundColor: colors.cardGreen,
+      backgroundColor: colors.cardGreen
     }
   },
   {
     content: '축하해',
     style: {
       transform: 'rotate(2deg)',
-      backgroundColor: colors.cardWhite,
+      backgroundColor: colors.cardWhite
     }
   },
   {
     content: '응원해',
     style: {
       transform: 'rotate(10deg)',
-      backgroundColor: colors.cardBlue,
+      backgroundColor: colors.cardBlue
     }
-  },
+  }
 ]
 
 const Home = () => {
-  console.log(colors);
   return (
     <div className={styles.home}>
       <div>
@@ -80,20 +79,17 @@ const Home = () => {
       </div>
       <div className={styles['card-container']}>
         {cardOption.map(({ content, style }) => (
-          <Card
-            key={content}
-            content={content}
-            cardStyle={style}
-          />
+          <Card key={content} content={content} cardStyle={style} />
         ))}
       </div>
       <div className={styles['home-description']}>
-        추억의 롤링페이퍼에서<br />
+        추억의 롤링페이퍼에서
+        <br />
         추억을 만들고 간직하세요!
       </div>
       <KakaoButton />
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
