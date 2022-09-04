9e2375a3d0f42148763adf12caae6439978c8434
jewonyeon <yjw9852@gmail.com>
2022-09-04 18:55:20 +0900

    #20 feat: api 업데이트

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,77 +1,15 @@
-import { axiosClient } from ".";
+import { axiosClient } from '.'
 
-const SIGNUP = "user/signup";
-const LOGIN = "member/login";
-const NICKNAME = "member";
+const NICKNAME = 'member'
 
-export const login_API = async (username: string | null) => {
-  try {
-    if (!username) return;
-    const data = { username };
-    return await axiosClient.post(LOGIN, data);
-  } catch (error) {
-    console.log(error, "error");
-  }
-};
+export const KAKAO_LOGIN_URL = `${import.meta.env.VITE_BASE_URL}/oauth2/authorization/kakao`
 
 export const nickname_API = async (nickname: string | null) => {
   try {
-    if (!nickname) return;
-    const data = { nickname };
-    return await axiosClient.put(NICKNAME, data);
+    if (!nickname) return
+    const data = { nickname }
+    return await axiosClient.put(NICKNAME, data)
   } catch (error) {
-    console.log(error, "error");
+    console.log(error, 'error')
   }
-};
-
-export const singup_API = async (userData: kakaoProfile | null) => {
-  try {
-    return await axiosClient.post(SIGNUP, userData);
-  } catch (err: unknown) {
-    console.log(err, "err");
-  }
-};
-
-export interface kakaoToken {
-  access_token: string;
-  expires_in: number;
-  id_token: string;
-  refresh_token: string;
-  refresh_token_expires_in: number;
-  scope: string;
-  token_type: string;
-}
-
-export interface kakaoServerRes {
-  id: number;
-  kakao_account: {
-    email: string;
-    email_needs_agreement: boolean;
-    has_email: boolean;
-    is_email_valid: boolean;
-    is_email_verified: boolean;
-    profile: {
-      nickname: string;
-      thumbnail_image_url: string;
-      profile_image_url: string;
-      is_default_image: boolean;
-    };
-    profile_image_needs_agreement: boolean;
-    profile_nickname_needs_agreement: boolean;
-  };
-  properties: {
-    nickname: string;
-    profile_image: string;
-    thumbnail_image: string;
-  };
-}
-
-export interface kakaoProfile {
-  id: number;
-  profile: {
-    nickname: string;
-    thumbnail_image_url: string;
-    profile_image_url: string;
-    is_default_image: boolean;
-  };
 }
