ba8bf9c7fae45a2555adba7462278298e4004626
zellytozelly <zellytozelly@gmail.com>
2022-09-20 22:59:29 +0900

    #46 fix: 마이페이지 드롭다운 수정항목 추가

--- src/pages/MyPage/MyPageItem/MyPageDropdown/index.tsx
+++ src/pages/MyPage/MyPageItem/MyPageDropdown/index.tsx
@@ -4,16 +4,22 @@ const MyPageDropDown = ({ isVisible }: { isVisible: boolean }) => {
   const handleClickShare = () => {
     confirm('공유하기를 진행하시겠습니까?')
   }
+  const handleClickEdit = () => {
+    confirm('공유하기를 진행하시겠습니까?')
+  }
   const handleClickDelete = () => {
     confirm('삭제하기를 진행하시겠습니까?')
   }
   return (
     <section className={styles.dropdown}>
       <button type="button" onClick={handleClickShare}>
-        공유하기
+        공유 하기
+      </button>
+      <button type="button" onClick={handleClickEdit}>
+        수정 하기
       </button>
       <button type="button" onClick={handleClickDelete}>
-        삭제하기
+        삭제 하기
       </button>
       <button type="button">{isVisible ? '모두 보기' : '나만 보기'}</button>
     </section>
