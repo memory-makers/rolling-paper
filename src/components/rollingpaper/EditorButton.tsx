6268d4ac07f8e4e67ed89757f62b4a35b6ca0dc3
YumYumNyang <khw121699@gmail.com>
2022-09-25 18:05:20 +0900

    #53 fix: 롤링페이퍼 페이지 버튼 수정

--- src/components/rollingpaper/EditorButton.tsx
+++ src/components/rollingpaper/EditorButton.tsx
@@ -5,7 +5,7 @@ import styles from './rollingpaper.module.scss'
 const EditorButton = () => {
   const navigate = useNavigate()
   return (
-    <button className={styles['goto-login-btn']} onClick={() => navigate('/')}>
+    <button className={styles['editor-btn']} onClick={() => navigate('/editor')}>
       나도 써볼까?
     </button>
   )
