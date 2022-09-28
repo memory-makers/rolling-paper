0c049bd3c21f698ab6d3dbdd3d6ea7616ab89c2f
zellytozelly <zellytozelly@gmail.com>
2022-09-29 01:03:54 +0900

    #67 feat: 캡슐 로딩 컴포넌트

--- src/pages/ModalTest/ModalTest.tsx
+++ src/pages/ModalTest/ModalTest.tsx
@@ -2,6 +2,7 @@ import { ChangeEventHandler, MouseEventHandler, useState } from 'react'
 
 import Modal from '@/components/Modal'
 import { ModalButton, ModalInput, ModalText } from '@/components/Modal/ModalItem'
+import CapsuleSkeleton from '@/components/Skeleton/CapsuleSkeleton'
 
 const ModalTest = () => {
   const [, setIsModalOpen] = useState(false)
@@ -21,28 +22,29 @@ const ModalTest = () => {
   }
 
   return (
-    <Modal setIsModalOpen={setIsModalOpen}>
-      <ModalText type="title">롤링 페이퍼를 만들어볼까요?</ModalText>
+    <CapsuleSkeleton />
+    // <Modal setIsModalOpen={setIsModalOpen}>
+    //   <ModalText type="title">롤링 페이퍼를 만들어볼까요?</ModalText>
 
-      <ModalText type="label">롤링페이퍼 이름을 적어주세요</ModalText>
-      <ModalInput type="text" name="title" value="" onChange={handleTitleChange} />
+    //   <ModalText type="label">롤링페이퍼 이름을 적어주세요</ModalText>
+    //   <ModalInput type="text" name="title" value="" onChange={handleTitleChange} />
 
-      <ModalText type="label">언제 열어보시겠어요?</ModalText>
-      <ModalInput type="date" name="dueDate" value="" onChange={handleDueDateChange} />
+    //   <ModalText type="label">언제 열어보시겠어요?</ModalText>
+    //   <ModalInput type="date" name="dueDate" value="" onChange={handleDueDateChange} />
 
-      <ModalButton type="button" onClick={handleButtonClick} color="secondary">
-        취소
-      </ModalButton>
-      <ModalButton type="button" onClick={handleButtonClick}>
-        완료
-      </ModalButton>
-      <ModalButton type="button" onClick={handleButtonClick} size="small" color="secondary">
-        취소
-      </ModalButton>
-      <ModalButton type="button" onClick={handleButtonClick} size="small">
-        삭제
-      </ModalButton>
-    </Modal>
+    //   <ModalButton type="button" onClick={handleButtonClick} color="secondary">
+    //     취소
+    //   </ModalButton>
+    //   <ModalButton type="button" onClick={handleButtonClick}>
+    //     완료
+    //   </ModalButton>
+    //   <ModalButton type="button" onClick={handleButtonClick} size="small" color="secondary">
+    //     취소
+    //   </ModalButton>
+    //   <ModalButton type="button" onClick={handleButtonClick} size="small">
+    //     삭제
+    //   </ModalButton>
+    // </Modal>
   )
 }
 
