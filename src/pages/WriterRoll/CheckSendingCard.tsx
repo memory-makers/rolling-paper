0e35087bb2f2ac206ee3c87c03fd2d94da2f4955
zellytozelly <zellytozelly@gmail.com>
2022-10-03 00:01:40 +0900

    #67 feat: 카드 체크 모달 메시지 변경

--- src/pages/WriterRoll/CheckSendingCard.tsx
+++ src/pages/WriterRoll/CheckSendingCard.tsx
@@ -18,22 +18,28 @@ const CheckSendingCard = ({ setIsModalOpen }: Props) => {
   }
   const handleSendClick = () => {
     // console.log('카드 전송하기 클릭')
-    setIsFail(true)
+    // setIsFail(true)
     setIsModalOpen(false)
     navigate('/sending')
   }
 
   return (
     <Modal setIsModalOpen={setIsModalOpen}>
+      <p className={styles.subText}>* 작성한 카드는 전체 공개됩니다.</p>
       <ModalText type="title">
-        전송하면 수정할 수 없어요! <br /> 정말로 전송할까요?
+        한번 보내면 수정할 수 없어요 <br /> 정말로 카드를 보낼까요?
       </ModalText>
-      {isFail && <p className={styles.errorText}>전송 실패ㅠㅠ 잠시후 다시 시도해 주세요.</p>}
+      {isFail && (
+        <p className={styles.errorText}>
+          이런! 문제가 생겼어요. <br />
+          잠시후 다시 시도해 주세요!
+        </p>
+      )}
       <ModalButton type="button" size="small" color="secondary" onClick={handleCancelClick}>
         취소
       </ModalButton>
       <ModalButton type="button" size="small" onClick={handleSendClick}>
-        전송
+        보내기
       </ModalButton>
     </Modal>
   )
