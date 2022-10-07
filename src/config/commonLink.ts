28515c2dec48fdf5bc1b0f2532646b4649f24df8
zellytozelly <zellytozelly@gmail.com>
2022-10-07 20:26:01 +0900

    #80 feat: 프론트 도메인 변경

--- src/config/commonLink.ts
+++ src/config/commonLink.ts
@@ -1,4 +1,4 @@
-export const CLIENT_URL = 'https://rolling-pager-client.vercel.app'
+export const CLIENT_URL = 'https://rolling-paper-makers.vercel.app'
 export const CLIENT_PAPER_URL = `${CLIENT_URL}/rollingpaper/`
 
 export default { CLIENT_URL, CLIENT_PAPER_URL }
